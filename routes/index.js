b545d4d083eab733f594e0c3c9dd523f7413a2cc
lucas <cerfectus@gmail.com>
2018-10-16 21:33:20 -0500

    don

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router  = express.Router();
+const mailer = require("../helpers/mailer");
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+router.post("/", (req, res) => {
+  const options = req.body;
+  options.filename = "verify",
+  mailer.send(options)
+  .then(() => {
+    res.status(200).send("el corre se ha enviadow")
+  })
+  .catch(err =>{
+    console.log("algo salio mal", err);
+    res.status(500).json({err, "msg": "algo"})
+    })
+})
+
+module.exports = router;
